a53543106b41aaaa369451b861e48b487f99aa54
Ryan Cook <ryancook12@gmail.com>
2021-06-08 17:42:58 -0400

    Platform component

--- App.js
+++ App.js
@@ -1,24 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, Platform, Alert, View, Image, SafeAreaView, Button } from 'react-native';
 
 export default function App() {
 const handlePress = () => console.log("Text pressed")
 console.log(require('./assets/icon.png'))
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>Hello React Native</Text>
-      <TouchableOpacity onPress={() => console.log("image tapped")}>
-      <Image
-      blurRadius={1} 
-        source={{
-          width: 200,
-          height: 300,
-          uri: 'https:picsum.photos/200/300'
-          }} 
-        />
-      </TouchableOpacity>
-      <StatusBar style="auto" />
+      <Button title={"Click Me"} onPress={() => Alert.alert("Button Title", "Button message",
+      [ { text: "Yes"},
+        { text: "No" },
+        { text: "Third Option"}
+        ])
+        }
+      />
     </SafeAreaView>
   );
 }
@@ -27,7 +22,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: "center",
-    alignItems: "center",
+    paddingTop: Platform.OS === 'android' ? 20 : 0
   },
 });
