bbb6b782b4a985d9131654dc071afc15060aa636
Ryan Cook <ryancook12@gmail.com>
2021-06-10 16:46:08 -0400

    inputs

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
 import React from "react";
-
-import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  return <AccountScreen />;
+  return <ListingEditScreen />;
 }
